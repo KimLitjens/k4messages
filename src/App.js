22b7ad3ff62e5b51e7457d72bcaaa2e3cee3b51b
Kim Litjens <kimlitjens4@gmail.com>
2022-01-24 11:35:14 +0100

    add tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>K4 Messages</h1>
+      <h1 className="text-center text-3xl font-bold underline">K4 Messages</h1>
     </div>
   );
 }
