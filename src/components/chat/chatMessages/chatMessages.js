2f85e4b1e6f3403312d57bc32bbe6cfe17670bae
Kim Litjens <kimlitjens4@gmail.com>
2022-02-01 14:54:01 +0100

    add scroll to chatMessages

--- src/components/chat/chatMessages/chatMessages.js
+++ src/components/chat/chatMessages/chatMessages.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ChatMessages({ chat, getTimeInHoursAndMinutes }) {
     return (
-        <div className="border border-red-500 w-11/12 h-96">
+        <div className="overflow-auto border border-red-500 w-11/12 h-96">
             {chat.messages !== undefined && chat.messages.map(message => <div key={message.time} >
                 <div className="flex">
                     <h2 className="mx-4">{message.senderName}</h2>
