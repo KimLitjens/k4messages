6ebaa4f16c0236e2eab53b0bbf2cde0b76ba081e
Kim Litjens <kimlitjens4@gmail.com>
2022-02-03 19:53:29 +0100

    changed chatheader when no chat selected

--- src/components/chat/chatHeader/chatHeader.js
+++ src/components/chat/chatHeader/chatHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function ChatHeader({ userName, receiver }) {
-    return <div><h2>{receiver ? receiver.username : userName}</h2></div>;
+    return <div><h2>{receiver ? receiver.username : `Welcom ${userName}`}</h2></div>;
 }
