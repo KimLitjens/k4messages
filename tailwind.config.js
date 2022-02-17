edb4913a63df3a38a64caa841de35ab68cb5eaf9
Kim Litjens <kimlitjens4@gmail.com>
2022-02-17 15:35:06 +0100

    set tailwind collor standards

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'first': '#222831',
+        'second': '#393e46',
+        'third': '#91A3B0',
+        'accent': ' #f96d00',
+        'secondAccent': '#f2f2f2',
+      },
+    },
   },
   plugins: [],
 }
